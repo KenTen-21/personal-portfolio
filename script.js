80a6fcf8f0b1e33031fcdb5d5c35344e6483f7f5
Kenneth Leung <132963840+KenTen-21@users.noreply.github.com>
2026-02-08 22:06:48 -0800

    For Personal Portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById('hamburger-btn');
+const navbar = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
+// Close menu when a link is clicked
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', () => {
+        navbar.classList.remove('active');
+    });
+});
